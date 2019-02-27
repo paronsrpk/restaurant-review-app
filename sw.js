e5901151c4d22518b08c49e15d8d222cf65c0327
paronsrpk <paronsrpk@gmail.com>
2019-02-27 23:31:44 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+const cacheName = 'restaurant-review';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        '/',
+        'index.html',
+        'restaurant.html',
+        'js/main.js',
+        'js/dbhelper.js',
+        'js/restaurant_info.js',
+        'data/restaurants.json',
+        'css/styles.css',
+        'css/responsive.css',
+        'img/1.jpg',
+        'img/2.jpg',
+        'img/3.jpg',
+        'img/4.jpg',
+        'img/5.jpg',
+        'img/6.jpg',
+        'img/7.jpg',
+        'img/8.jpg',
+        'img/9.jpg',
+        'img/10.jpg',
+        'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+        'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js'
+
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
